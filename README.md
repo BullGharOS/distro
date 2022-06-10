ad99e98d7a99ca876f6d813b296a841cf4610416
nikiarsov777 <nikiarsov@gmail.com>
2022-06-10 23:30:14 +0300

    large files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# distro
+version https://git-lfs.github.com/spec/v1
+oid sha256:13f13789410ece9c921c9d1a303752bd4f4590ff82374b4493519c8d92cd1b5c
+size 8
--- make-cd-live.sh
+++ make-cd-live.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
-grub-mkrescue -iso-level 3 -o live-cd.iso CD/
+version https://git-lfs.github.com/spec/v1
+oid sha256:d3dcbe64c9942a4a88f79deef2ec69137c53938d5b2ebb9e74f899c21ade7b5c
+size 58
